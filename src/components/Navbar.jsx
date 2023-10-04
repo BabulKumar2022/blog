f814e80ab52d9bcfcbdeecb39a864800fd2c132f
unknown <bb3583400@gmail.com>
2023-10-04 17:32:29 +0600

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
+      <h1 className="text-xl font-extrabold"><Link to="/">Blog</Link> </h1>
+      <div className="flex items-center justify-center space-x-2 md:space-x-4">
+        <h3><Link to="/login"> Login</Link></h3>
+        <h3><Link to="/register">Register</Link></h3>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+
+
+const Home = () => {
+  return (
+    <div className="">
+      <h1 className="text-3xl font-bold underline bg-[#1da1f2]">
+      HOME
+    </h1>
+    </div>
+  )
+}
+
+export default Home
