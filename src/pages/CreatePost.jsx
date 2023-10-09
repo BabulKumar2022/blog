d049c2cb29cd022b4070a2f226dd78f729879ffb
unknown <bb3583400@gmail.com>
2023-10-09 23:37:14 +0600

    write

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -10,6 +10,21 @@ const CreatePost = () => {
             <form className="w-full flex flex-col space-y-4 md:space-y-8">
                 <input type="text" placeholder="Enter Post Title" className="px-4 py-2 outline-none"/> 
                 <input type="file"  className="px-4 py-2 "/>
+                <div className="flex flex-col">
+                  <div className="flex items-center space-x-4 md:space-x-8">
+                    <input type="text" className="px-4 py-2 outline-none" placeholder="enter post category" />
+                    <div className="bg-black text-white px-4 py-2 font-semibold cursor-pointer">Add</div>
+                  </div>
+                  <div className="flex px-4 mt-3">
+                    <div className="flex justify-center items-center space-x-2 mr-4 bg-gray-200 px-2 py-1 rounded-md:">
+                      <p>Tech</p>
+                    </div>
+                    <div className="flex justify-center items-center space-x-2 mr-4 bg-gray-200 px-2 py-1 rounded-md:">
+                      <p>Tech</p>
+                    </div>
+
+                  </div>
+                </div>
             </form>
        </div>
        <Footer/>
